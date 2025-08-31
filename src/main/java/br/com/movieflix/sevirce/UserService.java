2f976c30c36b5d975428d735fd4a44e4da9b27b5
Lucasstq <fit.luk@outlook.com>
2025-08-31 15:48:33 -0300

    feat: Cria um AuthController para rotas de registro e login do usuario e também modifica o UserService para criptografia de senha

--- src/main/java/br/com/movieflix/sevirce/UserService.java
+++ src/main/java/br/com/movieflix/sevirce/UserService.java
@@ -3,6 +3,7 @@ package br.com.movieflix.sevirce;
 import br.com.movieflix.entity.User;
 import br.com.movieflix.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,9 +11,12 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public User save(User savedUser) {
-        return userRepository.save(savedUser);
+    public User save(User user) {
+        String password = user.getPassword();
+        user.setPassword(passwordEncoder.encode(password));
+        return userRepository.save(user);
     }
 
 }

7483fa6f84f59bd3a9fd5403093d497e5ff883ae
Lucasstq <fit.luk@outlook.com>
2025-08-31 12:27:45 -0300

    feat: Cria a entidade de usuário e também implementa a sua migration

---
+++ src/main/java/br/com/movieflix/entity/User.java
@@ -0,0 +1,29 @@
+package br.com.movieflix.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(nullable = false)
+    String name;
+
+    @Column(nullable = false, unique = true)
+    String email;
+
+    @Column(nullable = false)
+    String password;
+
+}
